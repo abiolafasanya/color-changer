57ca13ed62416ce788b9c4d2b201e4ebfe50bcbe
abiolafasanya <hezekiahfasanya@gmail.com>
2023-08-24 00:46:03 +0100

    vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 function App() {
-const [background, setBackground] = useState('#ffffff');
-function generateRandomColor() {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-   setBackground("#" + randomColor);
-}
+  const [background, setBackground] = useState("#ffffff");
+  function generateRandomColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    setBackground("#" + randomColor);
+  }
   return (
-  <div>
-    <Header />
-    <main style={{backgroundColor: background}} onClick={generateRandomColor}>
-      <h2>Background Color: {background}</h2>
-      <button>Change Color</button>
-    </main>
-  </div>
-  )
+    <div>
+      <Header />
+      <main
+        style={{ backgroundColor: background }}
+        onClick={generateRandomColor}
+      >
+        <h2>Background Color: {background}</h2>
+        <button>Change Color</button>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <header>
-        <h2>Color Flipper</h2>
-        <menu className='content'>
-            <a href='#'>Simple</a>
-            <a href='#'>Hex</a>
-        </menu>
+      <h2>Color Flipper</h2>
+      <menu className="content">
+        <a href="#">Simple</a>
+        <a href="#">Hex</a>
+      </menu>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

15be96cb5f39ab088fd78a8ef5613832a1502ce3
abiolafasanya <hezekiahfasanya@gmail.com>
2023-08-24 00:16:12 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+function App() {
+const [background, setBackground] = useState('#ffffff');
+function generateRandomColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+   setBackground("#" + randomColor);
+}
+  return (
+  <div>
+    <Header />
+    <main style={{backgroundColor: background}} onClick={generateRandomColor}>
+      <h2>Background Color: {background}</h2>
+      <button>Change Color</button>
+    </main>
+  </div>
+  )
+}
+
+export default App
